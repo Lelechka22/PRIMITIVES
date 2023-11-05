ccc7e16983a5a1e72d5cc24409a62a06c77a7fc9
Olga lomakina <lelechka.oa@gmail.com>
2023-11-05 20:14:40 +0400

    first

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 24656;
+        int rate = 20;
+
+        int bonus = price / rate;
+
+        System.out.println("Бонус: " + bonus);
+    }
+}
